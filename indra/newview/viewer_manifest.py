2662de75136f1ad5f405c96c76ba55c67d505693
Drake Arconis <lightdrake@gmail.com>
2013-06-01 06:38:00 -0400

    changed: Copy per skin syntax ini

--- indra/newview/viewer_manifest.py
+++ indra/newview/viewer_manifest.py
@@ -130,6 +130,7 @@ class ViewerManifest(LLManifest):
                     self.path("*/xui/*/*.xml")
                     self.path("*/xui/*/widgets/*.xml")
                     self.path("*/*.xml")
+                    self.path("*/*.ini")
 
                     # Local HTML files (e.g. loading screen)
                     # The claim is that we never use local html files any

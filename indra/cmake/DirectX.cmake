005989bc3307a0934e98ca305b9d3edb128f31ad
Drake Arconis <lightdrake@gmail.com>
2013-07-11 12:46:35 -0400

    qtcreator and vs2012 torture

--- indra/cmake/DirectX.cmake
+++ indra/cmake/DirectX.cmake
@@ -1,25 +1,20 @@
 # -*- cmake -*-
 
 if (WINDOWS)
-  if (MSVC10)
-    find_path(DIRECTX_INCLUDE_DIR dxdiag.h
-              "$ENV{DXSDK_DIR}/Include"
-              "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (June 2010)/Include"
-              "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (August 2009)/Include"
-              "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (March 2009)/Include"
-              "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (August 2008)/Include"
-              "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (June 2008)/Include"
-              "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (March 2008)/Include"
-              "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (November 2007)/Include"
-              "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (August 2007)/Include"
-              "C:/DX90SDK/Include"
-              "$ENV{PROGRAMFILES}/DX90SDK/Include"
-              )
-  elseif (MSVC11)
-    find_path(DIRECTX_INCLUDE_DIR dxdiag.h
-              "$ENV{WindowsSdkDir}/Include/um"
-              )
-  endif (MSVC10)
+  find_path(DIRECTX_INCLUDE_DIR dxdiag.h
+            "$ENV{DXSDK_DIR}/Include"
+            "$ENV{WindowsSdkDir}/Include/um"
+            "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (June 2010)/Include"
+            "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (August 2009)/Include"
+            "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (March 2009)/Include"
+            "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (August 2008)/Include"
+            "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (June 2008)/Include"
+            "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (March 2008)/Include"
+            "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (November 2007)/Include"
+            "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (August 2007)/Include"
+            "C:/DX90SDK/Include"
+            "$ENV{PROGRAMFILES}/DX90SDK/Include"
+            )
 
   if (DIRECTX_INCLUDE_DIR)
     include_directories(${DIRECTX_INCLUDE_DIR})
@@ -30,25 +25,20 @@ if (WINDOWS)
     message(FATAL_ERROR "Could not find DirectX SDK Include")
   endif (DIRECTX_INCLUDE_DIR)
 
-  if (MSVC10)
-    find_path(DIRECTX_LIBRARY_DIR dxguid.lib
-              "$ENV{DXSDK_DIR}/Lib/x86"
-              "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (June 2010)/Lib/x86"
-              "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (August 2009)/Lib/x86"
-              "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (March 2009)/Lib/x86"
-              "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (August 2008)/Lib/x86"
-              "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (June 2008)/Lib/x86"
-              "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (March 2008)/Lib/x86"
-              "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (November 2007)/Lib/x86"
-              "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (August 2007)/Lib/x86"
-              "C:/DX90SDK/Lib"
-              "$ENV{PROGRAMFILES}/DX90SDK/Lib"
-              )
-  elseif (MSVC11)
-    find_path(DIRECTX_LIBRARY_DIR dxguid.lib
-              "$ENV{WindowsSdkDir}/Lib/win8/um/x86"
-              )
-  endif (MSVC10)
+  find_path(DIRECTX_LIBRARY_DIR dxguid.lib
+            "$ENV{DXSDK_DIR}/Lib/x86"
+            "$ENV{WindowsSdkDir}/Lib/win8/um/x86"
+            "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (June 2010)/Lib/x86"
+            "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (August 2009)/Lib/x86"
+            "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (March 2009)/Lib/x86"
+            "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (August 2008)/Lib/x86"
+            "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (June 2008)/Lib/x86"
+            "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (March 2008)/Lib/x86"
+            "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (November 2007)/Lib/x86"
+            "$ENV{PROGRAMFILES}/Microsoft DirectX SDK (August 2007)/Lib/x86"
+            "C:/DX90SDK/Lib"
+            "$ENV{PROGRAMFILES}/DX90SDK/Lib"
+            )
 
   if (DIRECTX_LIBRARY_DIR)
     if (DIRECTX_FIND_QUIETLY)

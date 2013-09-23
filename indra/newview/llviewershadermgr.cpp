0e4aa6d88aaaeda180acc14d1b3f94f17031948c
Drake Arconis <lightdrake@gmail.com>
2013-09-22 22:31:39 -0400

    added: Tofu SSR

--- indra/newview/llviewershadermgr.cpp
+++ indra/newview/llviewershadermgr.cpp
@@ -1771,7 +1771,7 @@ BOOL LLViewerShaderMgr::loadShadersDeferred()
 		gDeferredSoftenProgram.mShaderFiles.clear();
 		gDeferredSoftenProgram.mShaderFiles.push_back(make_pair("deferred/softenLightV.glsl", GL_VERTEX_SHADER_ARB));
 		gDeferredSoftenProgram.mShaderFiles.push_back(make_pair("deferred/softenLightF.glsl", GL_FRAGMENT_SHADER_ARB));
-
+		gDeferredSoftenProgram.addPermutation("USE_SSR", (bool)gSavedSettings.getBOOL("RenderDeferredSSR") ? "1" : "0");
 		gDeferredSoftenProgram.mShaderLevel = mVertexShaderLevel[SHADER_DEFERRED];
 
 		if (gSavedSettings.getBOOL("RenderDeferredSSAO"))

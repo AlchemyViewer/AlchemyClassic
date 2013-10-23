3836d747eb45340141f0c9c3cc65c873ba0172ee
Tarnix <kaelux@gmail.com>
2013-10-23 02:28:42 -0400

    fixed: we should make sure we select a valid attachment before clearing selection.

--- indra/newview/llinventorybridge.cpp
+++ indra/newview/llinventorybridge.cpp
@@ -5257,11 +5257,11 @@ void LLObjectBridge::performAction(LLInventoryModel* model, std::string action)
 	}
 	else if ("edit_object" == action)
 	{
-		LLSelectMgr::getInstance()->clearSelections(); // Clear the current selection first.
 		if (LLInventoryItem* itemp = gInventory.getItem(mUUID))
 		{
 			if (LLViewerObject* objectp = gAgentAvatarp->getWornAttachment(itemp->getLinkedUUID()))
 			{
+				LLSelectMgr::getInstance()->clearSelections(); // Clear the current selection first.
 				LLFloaterReg::showInstance("build");
 				LLToolMgr::getInstance()->setCurrentToolset(gBasicToolset);
 				LLToolMgr::getInstance()->getCurrentToolset()->selectTool(LLToolCompTranslate::getInstance());

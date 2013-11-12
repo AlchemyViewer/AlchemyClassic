5df8d472c030bd5cac3da98cff681390d258449b
Drake Arconis <lightdrake@gmail.com>
2013-11-12 02:09:44 -0500

    fixed: Cmake warning

--- indra/cmake/Copy3rdPartyLibs.cmake
+++ indra/cmake/Copy3rdPartyLibs.cmake
@@ -71,7 +71,7 @@ if(WINDOWS)
       else(WORD_SIZE STREQUAL 64)
         set(debug_files ${debug_files} fmodexL.dll)
         set(release_files ${release_files} fmodex.dll)
-      endif(WORD_SIZE STREQUAL 32)
+      endif(WORD_SIZE STREQUAL 64)
     endif (FMODEX)
 
 #*******************************

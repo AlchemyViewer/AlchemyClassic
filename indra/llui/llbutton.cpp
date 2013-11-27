2101eec5dde41cc3cc18af9e2a9ac31a9e125aeb
Drake Arconis <lightdrake@gmail.com>
2013-11-27 18:09:50 -0500

    changed: Make button highlight do go down yes up sideways Fix inspired by Firestorm/Ansariel

--- indra/llui/llbutton.cpp
+++ indra/llui/llbutton.cpp
@@ -642,8 +642,8 @@ void LLButton::draw()
 	bool selected = getToggleState();
 	
 	bool use_glow_effect = FALSE;
-	LLColor4 highlighting_color = LLColor4::white;
-	LLColor4 glow_color;
+	LLColor4 highlighting_color = LLUIColorTable::instance().getColor("ButtonHighlightColor", LLColor4::white); // <alchemy/>
+	LLColor4 glow_color = LLUIColorTable::instance().getColor("ButtonGlowColor", LLColor4::white); // <alchemy/>
 	LLRender::eBlendType glow_type = LLRender::BT_ADD_WITH_ALPHA;
 	LLUIImage* imagep = NULL;
 

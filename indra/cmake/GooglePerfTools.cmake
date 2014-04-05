ef94e08301127b9d7df9b004eadbe62a3d54e2cf
Drake Arconis <lightdrake@gmail.com>
2014-04-05 17:04:13 -0400

    Win64 Gperftools

--- indra/cmake/GooglePerfTools.cmake
+++ indra/cmake/GooglePerfTools.cmake
@@ -3,9 +3,7 @@ include(Prebuilt)
 
 # If you want to enable or disable TCMALLOC in viewer builds, this is the place.
 # set ON or OFF as desired.
-if (NOT WORD_SIZE EQUAL 64)
-  set (USE_TCMALLOC OFF CACHE BOOL "Build the viewer with google tcmalloc")
-endif (NOT WORD_SIZE EQUAL 64)
+set (USE_TCMALLOC OFF CACHE BOOL "Build the viewer with google tcmalloc")
 
 if (STANDALONE)
   include(FindGooglePerfTools)

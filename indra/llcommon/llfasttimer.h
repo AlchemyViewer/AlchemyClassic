b13389796ac753b18fb69ae85a281ab95b6612aa
Luminous Luminos <cryostasix@gmail.com>
2014-05-19 20:54:50 -0500

    fixed: Move mStartTime above the return so its always initalized Blame GCC 4.9, but it did caught a potential bug

--- indra/llcommon/llfasttimer.h
+++ indra/llcommon/llfasttimer.h
@@ -289,6 +289,8 @@ block_timer_tree_bf_iterator_t end_block_timer_tree_bf();
 LL_FORCE_INLINE BlockTimer::BlockTimer(BlockTimerStatHandle& timer)
 {
 #if LL_FAST_TIMER_ON
+	mStartTime = getCPUClockCount64();
+
 	BlockTimerStackRecord* cur_timer_data = LLThreadLocalSingletonPointer<BlockTimerStackRecord>::getInstance();
 	if (!cur_timer_data) return;
 	TimeBlockAccumulator& accumulator = timer.getCurrentAccumulator();
@@ -302,8 +304,6 @@ LL_FORCE_INLINE BlockTimer::BlockTimer(BlockTimerStatHandle& timer)
 	cur_timer_data->mActiveTimer = this;
 	cur_timer_data->mTimeBlock = &timer;
 	cur_timer_data->mChildTime = 0;
-
-	mStartTime = getCPUClockCount64();
 #endif
 }
 

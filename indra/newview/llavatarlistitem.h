9babea7cc3056b77afed461b4acfb330fead0050
Drake Arconis <lightdrake@gmail.com>
2014-06-12 03:09:54 -0400

    fixed: Correct virtual override handleVisibilityChange to onVisibilityChange

--- indra/newview/llavatarlistitem.h
+++ indra/newview/llavatarlistitem.h
@@ -84,7 +84,7 @@ public:
 	/**
 	 * Processes notification from speaker indicator to update children when indicator's visibility is changed.
 	 */
-    virtual void handleVisibilityChange ( BOOL new_visibility );
+    virtual void onVisibilityChange ( BOOL new_visibility ); // <alchemy/>
 	virtual S32	notifyParent(const LLSD& info);
 	virtual void onMouseLeave(S32 x, S32 y, MASK mask);
 	virtual void onMouseEnter(S32 x, S32 y, MASK mask);

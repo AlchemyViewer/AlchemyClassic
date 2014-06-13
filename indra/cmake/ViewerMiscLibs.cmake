afa8bc9799f78196ca363269265c2a0a633394d2
Cinder <cinder.roxley@phoenixviewer.com>
2014-06-13 08:50:33 -0600

    We need to use a 32-bit prebuilt slplugin to get webkit on mac64 since we only have a 32-bit qtwebkit blob

--- indra/cmake/ViewerMiscLibs.cmake
+++ indra/cmake/ViewerMiscLibs.cmake
@@ -6,5 +6,8 @@ if (NOT STANDALONE)
   use_prebuilt_binary(libuuid)
   use_prebuilt_binary(slvoice)
   use_prebuilt_binary(fontconfig)
+  if (DARWIN AND NOT WORD_SIZE EQUAL 32)
+    use_prebuilt_binary(slplugin_x86)
+  endif (DARWIN AND NOT WORD_SIZE EQUAL 32)
 endif(NOT STANDALONE)
 

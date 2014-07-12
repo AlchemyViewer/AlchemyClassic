9acf84d106383cfce2a369a6b50042a3d23e685e
Drake Arconis <lightdrake@gmail.com>
2014-07-11 23:25:46 -0400

    changed: Cache the flashtimer settings as they are called frequently

--- indra/llui/llflashtimer.cpp
+++ indra/llui/llflashtimer.cpp
@@ -43,10 +43,12 @@ LLFlashTimer::LLFlashTimer(callback_t cb, S32 count, F32 period)
 	// By default use settings from settings.xml to be able change them via Debug settings. See EXT-5973.
 	// Due to Timer is implemented as derived class from EventTimer it is impossible to change period
 	// in runtime. So, both settings are made as required restart.
-	mFlashCount = 2 * ((count > 0) ? count : LLUI::sSettingGroups["config"]->getS32("FlashCount"));
+	static LLUICachedControl<S32> sFlashCount("FlashCount", 8);
+	mFlashCount = 2 * ((count > 0) ? count : sFlashCount);
 	if (mPeriod <= 0)
 	{
-		mPeriod = LLUI::sSettingGroups["config"]->getF32("FlashPeriod");
+		static LLUICachedControl<F32> sFlashPeriod("FlashPeriod", 0.5f);
+		mPeriod = sFlashPeriod;
 	}
 }
 

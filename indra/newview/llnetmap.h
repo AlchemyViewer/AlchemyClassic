71142b0cd885b33b21fe511dbbdc9ae3e79182cc
Drake Arconis <lightdrake@gmail.com>
2014-08-05 19:18:47 -0400

    Switch from std::unordered_map to boost:unordered_map as boost is faster

--- indra/newview/llnetmap.h
+++ indra/newview/llnetmap.h
@@ -35,7 +35,7 @@
 #include "llpointer.h"
 #include "llcoord.h"
 
-#include <unordered_map>
+#include <boost/unordered_map.hpp>
 
 class LLColor4U;
 class LLImageRaw;
@@ -130,7 +130,7 @@ private:
 	std::string		mToolTipMsg;
 
 	
-	typedef std::unordered_map<LLUUID, LLColor4, LLUUIDHash> uuid_color_umap_t;
+	typedef boost::unordered_map<LLUUID, LLColor4, LLUUIDHash> uuid_color_umap_t;
 	uuid_color_umap_t mCustomColors;
 
 public:

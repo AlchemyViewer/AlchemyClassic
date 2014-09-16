83c75bd73b74bfb2d7678fdb64a9bb835de4f95e
Cinder <cinder@sdf.org>
2014-09-16 10:51:50 -0600

    ALCH-107 - Make Recent People persist between sessions

--- indra/newview/llpanelpeople.h
+++ indra/newview/llpanelpeople.h
@@ -118,6 +118,7 @@ private:
 	void					onNearbyViewSortMenuItemClicked(const LLSD& userdata);
 	void					onGroupsViewSortMenuItemClicked(const LLSD& userdata);
 	void					onRecentViewSortMenuItemClicked(const LLSD& userdata);
+	void					onRecentViewClearHistoryMenuItemClicked();
 
 	bool					onFriendsViewSortMenuItemCheck(const LLSD& userdata);
 	bool					onRecentViewSortMenuItemCheck(const LLSD& userdata);

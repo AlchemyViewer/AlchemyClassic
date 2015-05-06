2678aa2d5c4e6655b48f0a1695256e8303dbdc91
Drake Arconis <drake@alchemyviewer.org>
2015-05-06 17:54:55 -0400

    Fix some logically stupid code in LLScriptEditor

--- indra/newview/llscripteditor.cpp
+++ indra/newview/llscripteditor.cpp
@@ -119,13 +119,12 @@ void LLScriptEditor::drawLineNumbers()
 				const LLWString ltext = utf8str_to_wstring(llformat("%d", line.mLineNum ));
 				BOOL is_cur_line = cursor_line == line.mLineNum;
 				const U8 style = is_cur_line ? LLFontGL::BOLD : LLFontGL::NORMAL;
-				const LLColor4 fg_color = is_cur_line ? mFgColor : mFgColor;
 				num_font->render(
 								 ltext, // string to draw
 								 0, // begin offset
 								 UI_TEXTEDITOR_LINE_NUMBER_MARGIN - 2, // x
 								 line_bottom, // y
-								 fg_color,
+								 mFgColor,
 								 LLFontGL::RIGHT, // horizontal alignment
 								 LLFontGL::BOTTOM, // vertical alignment
 								 style,

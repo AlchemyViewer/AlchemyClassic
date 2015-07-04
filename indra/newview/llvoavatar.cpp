a6e7ab0629fdcd24f01402651b59936b252e5db2
Drake Arconis <drake@alchemyviewer.org>
2015-07-04 12:43:22 -0400

    Partial revert of a patch.

--- indra/newview/llvoavatar.cpp
+++ indra/newview/llvoavatar.cpp
@@ -3316,7 +3316,7 @@ bool LLVOAvatar::isVisuallyMuted()
 		}
 	}
 
-	return isInMuteList() || muted;
+	return muted || isInMuteList();
 }
 
 void	LLVOAvatar::forceUpdateVisualMuteSettings()
@@ -8475,7 +8475,7 @@ void LLVOAvatar::updateImpostors()
 
 BOOL LLVOAvatar::isImpostor()
 {
-	return (sUseImpostors && (mUpdatePeriod >= IMPOSTOR_PERIOD)) ? TRUE : FALSE;
+	return (sUseImpostors && (isVisuallyMuted() || (mUpdatePeriod >= IMPOSTOR_PERIOD))) || isInMuteList() ? TRUE : FALSE;
 }
 
 

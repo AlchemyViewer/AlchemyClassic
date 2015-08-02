6a315eab8ba8b90026069f2306a49be612ba19a1
Drake Arconis <drake@alchemyviewer.org>
2015-08-01 20:33:31 -0400

    ALCH-257 - Fix about floater viewer data loading

--- indra/newview/llappviewer.h
+++ indra/newview/llappviewer.h
@@ -85,10 +85,6 @@ public:
 
 	const LLOSInfo& getOSInfo() const { return mSysOSInfo; }
 
-	void setServerReleaseNotesURL(const std::string& url) { mServerReleaseNotesURL = url; }
-	LLSD getViewerInfo() const;
-	std::string getViewerInfoString() const;
-
 	// Report true if under the control of a debugger. A null-op default.
 	virtual bool beingDebugged() { return false; } 
 
@@ -255,8 +251,6 @@ private:
 	
 	LLOSInfo mSysOSInfo; 
 	bool mReportedCrash;
-
-	std::string mServerReleaseNotesURL;
 
 	// Thread objects.
 	static LLTextureCache* sTextureCache; 
--- indra/newview/llfloaterabout.cpp
+++ indra/newview/llfloaterabout.cpp
@@ -52,6 +52,7 @@
 #include "llglheaders.h"
 #include "llfloater.h"
 #include "llfloaterreg.h"
+#include "llfontfreetype.h"
 #include "llimagej2c.h"
 #include "llsys.h"
 #include "lltrans.h"
@@ -70,23 +71,12 @@ extern LLMemoryInfo gSysMemory;
 extern U32 gPacketsIn;
 
 ///----------------------------------------------------------------------------
-/// Class LLServerReleaseNotesURLFetcher
-///----------------------------------------------------------------------------
-class LLServerReleaseNotesURLFetcher : public LLHTTPClient::Responder
-{
-	LOG_CLASS(LLServerReleaseNotesURLFetcher);
-public:
-	static void startFetch();
-private:
-	/* virtual */ void httpCompleted();
-};
-
-///----------------------------------------------------------------------------
 /// Class LLFloaterAbout
 ///----------------------------------------------------------------------------
-class LLFloaterAbout 
+class LLFloaterAbout
 	: public LLFloater
 {
+	friend class LLServerReleaseNotesURLFetcher;
 	friend class LLFloaterReg;
 private:
 	LLFloaterAbout(const LLSD& key);
@@ -97,11 +87,46 @@ public:
 
 	/// Obtain the data used to fill out the contents string. This is
 	/// separated so that we can programmatically access the same info.
-	static LLSD getInfo();
+	static LLSD getInfo(const std::string& server_release_notes_url = "");
 	void onClickCopyToClipboard();
 
 private:
 	void setSupportText(const std::string& server_release_notes_url);
+};
+
+///----------------------------------------------------------------------------
+/// Class LLServerReleaseNotesURLFetcher
+///----------------------------------------------------------------------------
+class LLServerReleaseNotesURLFetcher : public LLHTTPClient::Responder
+{
+	LOG_CLASS(LLServerReleaseNotesURLFetcher);
+private:
+	/* virtual */ void httpCompleted()
+	{
+		LL_DEBUGS("ServerReleaseNotes") << dumpResponse()
+			<< " [headers:" << getResponseHeaders() << "]" << LL_ENDL;
+
+		LLFloaterAbout* floater_about = LLFloaterReg::getTypedInstance<LLFloaterAbout>("sl_about");
+		if (floater_about)
+		{
+			std::string location = getResponseHeader(HTTP_IN_HEADER_LOCATION);
+			if (location.empty())
+			{
+				location = LLTrans::getString("ErrorFetchingServerReleaseNotesURL");
+			}
+			floater_about->setSupportText(location);
+		}
+	}
+
+	/*virtual*/ void httpFailure()
+	{
+		LLFloaterAbout* floater_about = LLFloaterReg::getTypedInstance<LLFloaterAbout>("sl_about");
+		if (floater_about)
+		{
+			std::string location = LLTrans::getString("ErrorFetchingServerReleaseNotesURL");
+			floater_about->setSupportText(LLTrans::getString("ErrorFetchingServerReleaseNotesURL"));
+		}
+	}
 };
 
 
@@ -134,11 +159,17 @@ BOOL LLFloaterAbout::postBuild()
 
 	static const LLUIColor about_color = LLUIColorTable::instance().getColor("TextFgReadOnlyColor");
 
-	if (gAgent.getRegion())
+	LLViewerRegion* regionp = gAgent.getRegion();
+	if (regionp)
 	{
-		// start fetching server release notes URL
-		setSupportText(LLTrans::getString("RetrievingData"));
-		LLServerReleaseNotesURLFetcher::startFetch();
+		support_widget->setText(LLTrans::getString("RetrievingData"), LLStyle::Params().color(about_color));
+		// We cannot display the URL returned by the ServerReleaseNotes capability
+		// because opening it in an external browser will trigger a warning about untrusted
+		// SSL certificate.
+		// So we query the URL ourselves, expecting to find
+		// an URL suitable for external browsers in the "Location:" HTTP header.
+		std::string cap_url = regionp->getCapability("ServerReleaseNotes");
+		LLHTTPClient::get(cap_url, new LLServerReleaseNotesURLFetcher);
 	}
 	else // not logged in
 	{
@@ -196,9 +227,124 @@ BOOL LLFloaterAbout::postBuild()
 	return TRUE;
 }
 
-LLSD LLFloaterAbout::getInfo()
+LLSD LLFloaterAbout::getInfo(const std::string& server_release_notes_url)
 {
-	return LLAppViewer::instance()->getViewerInfo();
+	// The point of having one method build an LLSD info block and the other
+	// construct the user-visible About string is to ensure that the same info
+	// is available to a getInfo() caller as to the user opening
+	// LLFloaterAbout.
+	LLSD info;
+	LLSD version;
+	version.append(LLVersionInfo::getMajor());
+	version.append(LLVersionInfo::getMinor());
+	version.append(LLVersionInfo::getPatch());
+	version.append(LLVersionInfo::getBuild());
+	info["VIEWER_VERSION"] = version;
+	info["VIEWER_VERSION_STR"] = LLVersionInfo::getVersion();
+	info["BUILD_DATE"] = __DATE__;
+	info["BUILD_TIME"] = __TIME__;
+	info["CHANNEL"] = LLVersionInfo::getChannel();
+
+	// return a URL to the release notes for this viewer, such as:
+	// http://wiki.secondlife.com/wiki/Release_Notes/Second Life Beta Viewer/2.1.0.123456
+	std::string url = LLTrans::getString("RELEASE_NOTES_BASE_URL");
+	if (!LLStringUtil::endsWith(url, "/"))
+		url += "/";
+	url += LLURI::escape(LLVersionInfo::getChannel() + " " + LLVersionInfo::getVersion());
+	info["VIEWER_RELEASE_NOTES_URL"] = url;
+
+#if LL_MSVC
+	info["COMPILER"] = "MSVC";
+	info["COMPILER_VERSION"] = _MSC_FULL_VER; // <alchemy/>
+#elif LL_GNUC
+	info["COMPILER"] = "GCC";
+	info["COMPILER_VERSION"] = GCC_VERSION;
+#elif LL_CLANG
+	info["COMPILER"] = "Clang";
+	info["COMPILER_VERSION"] = __VERSION__;
+#elif LL_INTELC
+	info["COMPILER"] = "ICC";
+	info["COMPILER_VERSION"] = __ICC;
+#endif
+
+	// <alchemy>
+#if defined(_WIN64) || defined(__amd64__) || defined(__x86_64__)
+	info["BUILD_ARCH"] = "x64";
+#else
+	info["BUILD_ARCH"] = "x86";
+#endif
+	// </alchemy>
+
+	// Position
+	LLViewerRegion* region = gAgent.getRegion();
+	if (region)
+	{
+		LLVector3d pos = gAgent.getPositionGlobal();
+		info["POSITION"] = ll_sd_from_vector3d(pos);
+		info["POSITION_LOCAL"] = ll_sd_from_vector3(gAgent.getPosAgentFromGlobal(pos));
+		info["REGION"] = gAgent.getRegion()->getName();
+		info["HOSTNAME"] = gAgent.getRegion()->getHost().getHostName();
+		info["HOSTIP"] = gAgent.getRegion()->getHost().getString();
+		info["SERVER_VERSION"] = gLastVersionChannel;
+		LLSLURL slurl;
+		LLAgentUI::buildSLURL(slurl);
+		info["SLURL"] = slurl.getSLURLString();
+	}
+
+	// CPU
+	info["CPU"] = gSysCPU.getCPUString();
+	info["MEMORY_MB"] = LLSD::Integer(gSysMemory.getPhysicalMemoryKB().valueInUnits<LLUnits::Megabytes>());
+	// Moved hack adjustment to Windows memory size into llsys.cpp
+	info["OS_VERSION"] = LLAppViewer::instance()->getOSInfo().getOSString();
+	info["GRAPHICS_CARD_VENDOR"] = (const char*) (glGetString(GL_VENDOR));
+	info["GRAPHICS_CARD"] = (const char*) (glGetString(GL_RENDERER));
+
+#if LL_WINDOWS
+	LLSD driver_info = gDXHardware.getDisplayInfo();
+	if (driver_info.has("DriverVersion"))
+	{
+		info["GRAPHICS_DRIVER_VERSION"] = driver_info["DriverVersion"];
+	}
+#endif
+
+	info["OPENGL_VERSION"] = (const char*) (glGetString(GL_VERSION));
+	info["LIBCURL_VERSION"] = LLCurl::getVersionString();
+	info["J2C_VERSION"] = LLImageJ2C::getEngineInfo();
+	info["FONT_VERSION"] = LLFontFreetype::getVersionString();
+	bool want_fullname = true;
+	info["AUDIO_DRIVER_VERSION"] = gAudiop ? LLSD(gAudiop->getDriverName(want_fullname)) : LLSD();
+	if (LLVoiceClient::getInstance()->voiceEnabled())
+	{
+		LLVoiceVersionInfo version = LLVoiceClient::getInstance()->getVersion();
+		std::ostringstream version_string;
+		version_string << version.serverType << " " << version.serverVersion << std::endl;
+		info["VOICE_VERSION"] = version_string.str();
+	}
+	else
+	{
+		info["VOICE_VERSION"] = LLTrans::getString("NotConnected");
+	}
+
+	info["QT_WEBKIT_VERSION"] = "4.8.7 (version number hard-coded)";
+
+	S32 packets_in = LLViewerStats::instance().getRecording().getSum(LLStatViewer::PACKETS_IN);
+	if (packets_in > 0)
+	{
+		info["PACKETS_LOST"] = LLViewerStats::instance().getRecording().getSum(LLStatViewer::PACKETS_LOST);
+		info["PACKETS_IN"] = packets_in;
+		info["PACKETS_PCT"] = 100.f*info["PACKETS_LOST"].asReal() / info["PACKETS_IN"].asReal();
+	}
+
+	if (LLStringUtil::startsWith(server_release_notes_url, "http")) // it's an URL
+	{
+		info["SERVER_RELEASE_NOTES_URL"] = "[" + LLWeb::escapeURL(server_release_notes_url) + " " + LLTrans::getString("ReleaseNotes") + "]";
+	}
+	else
+	{
+		info["SERVER_RELEASE_NOTES_URL"] = server_release_notes_url;
+	}
+
+	return info;
 }
 
 class LLFloaterAboutListener: public LLEventAPI
@@ -241,18 +387,71 @@ void LLFloaterAbout::setSupportText(const std::string& server_release_notes_url)
 	getWindow()->incBusyCount();
 	getWindow()->setCursor(UI_CURSOR_ARROW);
 #endif
+	LLSD info(getInfo(server_release_notes_url));
 #if LL_WINDOWS
 	getWindow()->decBusyCount();
 	getWindow()->setCursor(UI_CURSOR_ARROW);
 #endif
 
-	LLViewerTextEditor *support_widget =
-		getChild<LLViewerTextEditor>("support_editor", true);
+	// Render the LLSD from getInfo() as a format_map_t
+	LLStringUtil::format_map_t args;
 
-	LLUIColor about_color = LLUIColorTable::instance().getColor("TextFgReadOnlyColor");
-	support_widget->clear();
-	support_widget->appendText(LLAppViewer::instance()->getViewerInfoString(),
-							   FALSE, LLStyle::Params() .color(about_color));
+	// allow the "Release Notes" URL label to be localized
+	args["ReleaseNotes"] = LLTrans::getString("ReleaseNotes");
+
+	for (LLSD::map_const_iterator ii(info.beginMap()), iend(info.endMap());
+	ii != iend; ++ii)
+	{
+		if (!ii->second.isArray())
+		{
+			// Scalar value
+			if (ii->second.isUndefined())
+			{
+				args[ii->first] = LLTrans::getString("none_text");
+			}
+			else
+			{
+				// don't forget to render value asString()
+				args[ii->first] = ii->second.asString();
+			}
+		}
+		else
+		{
+			// array value: build KEY_0, KEY_1 etc. entries
+			for (LLSD::Integer n(0), size(ii->second.size()); n < size; ++n)
+			{
+				args[STRINGIZE(ii->first << '_' << n)] = ii->second[n].asString();
+			}
+		}
+	}
+
+	// Now build the various pieces
+	std::ostringstream support;
+	support << LLTrans::getString("AboutHeader", args);
+	if (info.has("REGION"))
+	{
+		support << "\n\n" << LLTrans::getString("AboutPosition", args);
+	}
+	support << "\n\n" << LLTrans::getString("AboutSystem", args);
+	support << "\n";
+	if (info.has("GRAPHICS_DRIVER_VERSION"))
+	{
+		support << "\n" << LLTrans::getString("AboutDriver", args);
+	}
+	support << "\n" << LLTrans::getString("AboutLibs", args);
+	if (info.has("COMPILER"))
+	{
+		support << "\n" << LLTrans::getString("AboutCompiler", args);
+	}
+	if (info.has("PACKETS_IN"))
+	{
+		support << '\n' << LLTrans::getString("AboutTraffic", args);
+	}
+
+	LLViewerTextEditor *support_widget = getChild<LLViewerTextEditor>("support_editor", true);
+	static LLUIColor about_color = LLUIColorTable::instance().getColor("TextFgReadOnlyColor");
+	support_widget->setText(support.str(),
+							   LLStyle::Params().color(about_color));
 }
 
 ///----------------------------------------------------------------------------
@@ -264,40 +463,3 @@ void LLFloaterAboutUtil::registerFloater()
 		&LLFloaterReg::build<LLFloaterAbout>);
 
 }
-
-///----------------------------------------------------------------------------
-/// Class LLServerReleaseNotesURLFetcher implementation
-///----------------------------------------------------------------------------
-// static
-void LLServerReleaseNotesURLFetcher::startFetch()
-{
-	LLViewerRegion* region = gAgent.getRegion();
-	if (!region) return;
-
-	// We cannot display the URL returned by the ServerReleaseNotes capability
-	// because opening it in an external browser will trigger a warning about untrusted
-	// SSL certificate.
-	// So we query the URL ourselves, expecting to find
-	// an URL suitable for external browsers in the "Location:" HTTP header.
-	std::string cap_url = region->getCapability("ServerReleaseNotes");
-	LLHTTPClient::get(cap_url, new LLServerReleaseNotesURLFetcher);
-}
-
-// virtual
-void LLServerReleaseNotesURLFetcher::httpCompleted()
-{
-	LL_DEBUGS("ServerReleaseNotes") << dumpResponse() 
-									<< " [headers:" << getResponseHeaders() << "]" << LL_ENDL;
-
-	LLFloaterAbout* floater_about = LLFloaterReg::getTypedInstance<LLFloaterAbout>("sl_about");
-	if (floater_about)
-	{
-		std::string location = getResponseHeader(HTTP_IN_HEADER_LOCATION);
-		if (location.empty())
-		{
-			location = LLTrans::getString("ErrorFetchingServerReleaseNotesURL");
-		}
-		LLAppViewer::instance()->setServerReleaseNotesURL(location);
-	}
-}
-

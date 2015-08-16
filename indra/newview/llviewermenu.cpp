430006524aca5ee75e1ef50c81251b858bfe7469
Cinders <cinder@cinderblocks.biz>
2015-08-15 19:13:19 -0600

    Enable/Disable merchant shit in code vs xml because we need more refined logic

--- indra/newview/llviewermenu.cpp
+++ indra/newview/llviewermenu.cpp
@@ -401,22 +401,9 @@ void set_underclothes_menu_options()
 
 void set_merchant_SLM_menu()
 {
-    // DD-170 : SLM Alpha and Beta program : for the moment, we always show the SLM menu and 
-    // tools so that all merchants can try out the UI, even if not migrated.
-    // *TODO : Keep SLM UI hidden for non migrated merchant in released viewer
-    
-    //if (LLMarketplaceData::instance().getSLMStatus() == MarketplaceStatusCodes::MARKET_PLACE_NOT_MIGRATED_MERCHANT)
-    //{
-        // Merchant not migrated: show only the old Merchant Outbox menu
-    //    gMenuHolder->getChild<LLView>("MerchantOutbox")->setVisible(TRUE);
-    //}
-    //else
-    //{
-        // All other cases (new merchant, not merchant, migrated merchant): show the new Marketplace Listings menu and enable the tool
-        gMenuHolder->getChild<LLView>("MarketplaceListings")->setVisible(TRUE);
-        LLCommand* command = LLCommandManager::instance().getCommand("marketplacelistings");
-		gToolBarView->enableCommand(command->id(), true);
-    //}
+	gMenuHolder->getChild<LLView>("MarketplaceListings")->setVisible((BOOL)LLGridManager::getInstance()->isInSecondlife());
+	LLCommand* command = LLCommandManager::instance().getCommand("marketplacelistings");
+	gToolBarView->enableCommand(command->id(), LLGridManager::getInstance()->isInSecondlife());
 }
 
 void set_merchant_outbox_menu(U32 status, const LLSD& content)
@@ -425,7 +412,7 @@ void set_merchant_outbox_menu(U32 status, const LLSD& content)
     // In all other cases, we show it.
     if (status != MarketplaceErrorCodes::IMPORT_SERVER_API_DISABLED)
     {
-        gMenuHolder->getChild<LLView>("MerchantOutbox")->setVisible(TRUE);
+        gMenuHolder->getChild<LLView>("MerchantOutbox")->setVisible((BOOL)LLGridManager::getInstance()->isInSecondlife());
     }
 }
 

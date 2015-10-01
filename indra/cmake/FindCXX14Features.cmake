2688e0bd8c835e9383cce38f517448b88ee83385
Cinder <cinder@sdf.org>
2015-09-30 21:18:39 -0600

    Fix test for make_reverse_iterator

--- indra/cmake/FindCXX14Features.cmake
+++ indra/cmake/FindCXX14Features.cmake
@@ -109,15 +109,25 @@ CHECK_CXX_SOURCE_COMPILES("
 )
 
 CHECK_CXX_SOURCE_COMPILES("
-    #include <iterator>
-    template< class Iterator >
-    std::reverse_iterator<Iterator> make_reverse_iterator( Iterator i ) {
-      return std::reverse_iterator<Iterator>(i);
-    }
+	#include <iostream>
+	#include <iterator>
+	#include <vector>
+	#include <algorithm>
 
-    int main(void) {
-      return 0;
-    }
+	int main() {
+		auto v = std::vector<int>{ 1, 3, 10, 8, 22 };
+ 
+		std::sort(v.begin(), v.end());
+		std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, \", \"));
+ 
+		std::cout << '\n';
+ 
+		std::copy(
+			std::make_reverse_iterator(v.end()),
+			std::make_reverse_iterator(v.begin()),
+			std::ostream_iterator<int>(std::cout, \", \"));
+		return 0;
+}
 " HAVE_MAKE_REVERSE_ITERATOR
 )
 

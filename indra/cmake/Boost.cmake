c5ffd5b5b34181adbc3ff25b477a2d7007f950f7
Cinder <cinder@sdf.org>
2015-10-02 12:42:32 -0600

    Replace LLDate libapr with boost::posix_time

--- indra/cmake/Boost.cmake
+++ indra/cmake/Boost.cmake
@@ -9,75 +9,50 @@ if (USESYSTEMLIBS)
 
   set(BOOST_CONTEXT_LIBRARY boost_context-mt)
   set(BOOST_COROUTINE_LIBRARY boost_coroutine-mt)
+  set(BOOST_DATE_TIME_LIBRARY boost_date_time-mt)
   set(BOOST_FILESYSTEM_LIBRARY boost_filesystem-mt)
+  set(BOOST_IOSTREAMS_LIBRARY boost_iostreams-mt)
   set(BOOST_PROGRAM_OPTIONS_LIBRARY boost_program_options-mt)
   set(BOOST_REGEX_LIBRARY boost_regex-mt)
   set(BOOST_SIGNALS_LIBRARY boost_signals-mt)
   set(BOOST_SYSTEM_LIBRARY boost_system-mt)
   set(BOOST_THREAD_LIBRARY boost_thread-mt)
-  set(BOOST_IOSTREAMS_LIBRARY boost_iostreams-mt)
 else (USESYSTEMLIBS)
   use_prebuilt_binary(boost)
   set(Boost_INCLUDE_DIRS ${LIBS_PREBUILT_DIR}/include)
   set(BOOST_VERSION "1.59")
 
   if (WINDOWS)
-    if(MSVC80)
-      # This should be obsolete at this point
-      set(BOOST_CONTEXT_LIBRARY 
-          optimized libboost_context-vc80-mt-${BOOST_VERSION}
-          debug libboost_context-vc80-mt-gd-${BOOST_VERSION})
-      set(BOOST_FILESYSTEM_LIBRARY 
-          optimized libboost_filesystem-vc80-mt-${BOOST_VERSION}
-          debug libboost_filesystem-vc80-mt-gd-${BOOST_VERSION})
-      set(BOOST_PROGRAM_OPTIONS_LIBRARY 
-          optimized libboost_program_options-vc80-mt-${BOOST_VERSION}
-          debug libboost_program_options-vc80-mt-gd-${BOOST_VERSION})
-      set(BOOST_REGEX_LIBRARY
-          optimized libboost_regex-vc80-mt-${BOOST_VERSION}
-          debug libboost_regex-vc80-mt-gd-${BOOST_VERSION})
-      set(BOOST_SIGNALS_LIBRARY 
-          optimized libboost_signals-vc80-mt-${BOOST_VERSION}
-          debug libboost_signals-vc80-mt-gd-${BOOST_VERSION})
-      set(BOOST_SYSTEM_LIBRARY 
-          optimized libboost_system-vc80-mt-${BOOST_VERSION}
-          debug libboost_system-vc80-mt-gd-${BOOST_VERSION})
-      set(BOOST_THREAD_LIBRARY 
-          optimized libboost_thread-vc80-mt-${BOOST_VERSION}
-          debug libboost_thread-vc80-mt-gd-${BOOST_VERSION})
-      set(BOOST_IOSTREAMS_LIBRARY
-          optimized libboost_iostreams-vc80-mt-${BOOST_VERSION}
-          debug libboost_iostreams-vc80-mt-gd-${BOOST_VERSION})
-    else(MSVC80)
-      # MSVC 10.0 config
-      set(BOOST_CONTEXT_LIBRARY 
-          optimized libboost_context-mt
-          debug libboost_context-mt-gd)
-      set(BOOST_COROUTINE_LIBRARY 
-          optimized libboost_coroutine-mt
-          debug libboost_coroutine-mt-gd)
-      set(BOOST_FILESYSTEM_LIBRARY 
-          optimized libboost_filesystem-mt
-          debug libboost_filesystem-mt-gd)
-      set(BOOST_PROGRAM_OPTIONS_LIBRARY 
-          optimized libboost_program_options-mt
-          debug libboost_program_options-mt-gd)
-      set(BOOST_REGEX_LIBRARY
-          optimized libboost_regex-mt
-          debug libboost_regex-mt-gd)
-      set(BOOST_SIGNALS_LIBRARY 
-          optimized libboost_signals-mt
-          debug libboost_signals-mt-gd)
-      set(BOOST_SYSTEM_LIBRARY 
-          optimized libboost_system-mt
-          debug libboost_system-mt-gd)
-      set(BOOST_THREAD_LIBRARY 
-          optimized libboost_thread-mt
-          debug libboost_thread-mt-gd)
-      set(BOOST_IOSTREAMS_LIBRARY
-          optimized libboost_iostreams-mt
-          debug libboost_iostreams-mt-gd)
-    endif (MSVC80)
+    set(BOOST_CONTEXT_LIBRARY
+        optimized libboost_context-mt
+        debug libboost_context-mt-gd)
+    set(BOOST_COROUTINE_LIBRARY
+        optimized libboost_coroutine-mt
+        debug libboost_coroutine-mt-gd)
+    set(BOOST_DATE_TIME_LIBRARY
+        optimized libboost_date_time-mt
+        debug libboost_date_time-mt-gd)
+    set(BOOST_FILESYSTEM_LIBRARY
+        optimized libboost_filesystem-mt
+        debug libboost_filesystem-mt-gd)
+    set(BOOST_IOSTREAMS_LIBRARY
+        optimized libboost_iostreams-mt
+        debug libboost_iostreams-mt-gd)
+    set(BOOST_PROGRAM_OPTIONS_LIBRARY
+        optimized libboost_program_options-mt
+        debug libboost_program_options-mt-gd)
+    set(BOOST_REGEX_LIBRARY
+        optimized libboost_regex-mt
+        debug libboost_regex-mt-gd)
+    set(BOOST_SIGNALS_LIBRARY
+        optimized libboost_signals-mt
+        debug libboost_signals-mt-gd)
+    set(BOOST_SYSTEM_LIBRARY
+        optimized libboost_system-mt
+        debug libboost_system-mt-gd)
+    set(BOOST_THREAD_LIBRARY
+        optimized libboost_thread-mt
+        debug libboost_thread-mt-gd)
   elseif (LINUX)
     set(BOOST_CONTEXT_LIBRARY
         optimized boost_context-mt
@@ -85,9 +60,15 @@ else (USESYSTEMLIBS)
     set(BOOST_COROUTINE_LIBRARY
         optimized boost_coroutine-mt
         debug boost_coroutine-mt-d)
+    set(BOOST_DATE_TIME_LIBRARY
+        optimized boost_date_time-mt
+        debug boost_date_time-mt-d)
     set(BOOST_FILESYSTEM_LIBRARY
         optimized boost_filesystem-mt
         debug boost_filesystem-mt-d)
+    set(BOOST_IOSTREAMS_LIBRARY
+        optimized boost_iostreams-mt
+        debug boost_iostreams-mt-d)
     set(BOOST_PROGRAM_OPTIONS_LIBRARY
         optimized boost_program_options-mt
         debug boost_program_options-mt-d)
@@ -103,9 +84,6 @@ else (USESYSTEMLIBS)
     set(BOOST_THREAD_LIBRARY
         optimized boost_thread-mt
         debug boost_thread-mt-d)
-    set(BOOST_IOSTREAMS_LIBRARY
-        optimized boost_iostreams-mt
-        debug boost_iostreams-mt-d)
   elseif (DARWIN)
     set(BOOST_CONTEXT_LIBRARY
         optimized boost_context-mt
@@ -113,9 +91,15 @@ else (USESYSTEMLIBS)
     set(BOOST_COROUTINE_LIBRARY
         optimized boost_coroutine-mt
         debug boost_coroutine-mt-d)
+    set(BOOST_DATE_TIME_LIBRARY
+        optimized boost_date_time-mt
+        debug boost_date_time-mt-d)
     set(BOOST_FILESYSTEM_LIBRARY
         optimized boost_filesystem-mt
         debug boost_filesystem-mt-d)
+    set(BOOST_IOSTREAMS_LIBRARY
+        optimized boost_iostreams-mt
+        debug boost_iostreams-mt-d)
     set(BOOST_PROGRAM_OPTIONS_LIBRARY
         optimized boost_program_options-mt
         debug boost_program_options-mt-d)
@@ -131,8 +115,5 @@ else (USESYSTEMLIBS)
     set(BOOST_THREAD_LIBRARY
         optimized boost_thread-mt
         debug boost_thread-mt-d)
-    set(BOOST_IOSTREAMS_LIBRARY
-        optimized boost_iostreams-mt
-        debug boost_iostreams-mt-d)
-  endif (WINDOWS)
+  endif ()
 endif (USESYSTEMLIBS)

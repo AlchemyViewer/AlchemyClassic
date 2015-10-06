caa718e6967074e1bdd48b01eec94d38fe9a479e
Drake Arconis <drake@alchemyviewer.org>
2015-10-06 00:52:13 -0400

    More misc cleanup

--- indra/newview/llstartup.cpp
+++ indra/newview/llstartup.cpp
@@ -666,7 +666,7 @@ bool idle_startup()
 			if (gAudiop)
 			{
 #if LL_WINDOWS
-				// FMOD Ex on Windows needs the window handle to stop playing audio
+				// FMOD Studio on Windows needs the window handle to stop playing audio
 				// when window is minimized. JC
 				void* window_handle = (HWND)gViewerWindow->getPlatformWindow();
 #else

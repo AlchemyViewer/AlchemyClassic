506aeb9ae271c2a970233b89d416af1efc47422e
Drake Arconis <drake@alchemyviewer.org>
2016-01-03 18:10:41 -0500

    Remove files used for windows xp cef

--- indra/newview/viewer_manifest.py
+++ indra/newview/viewer_manifest.py
@@ -447,7 +447,6 @@ class WindowsManifest(ViewerManifest):
         # CEF runtime files - debug
         if self.args['configuration'].lower() == 'debug':
             if self.prefix(src=os.path.join(os.pardir, 'packages', 'bin', 'debug'), dst="llplugin"):
-                self.path("d3dcompiler_43.dll")
                 self.path("d3dcompiler_47.dll")
                 self.path("libcef.dll")
                 self.path("libEGL.dll")
@@ -461,7 +460,6 @@ class WindowsManifest(ViewerManifest):
         else:
         # CEF runtime files - not debug (release, relwithdebinfo etc.)
             if self.prefix(src=os.path.join(os.pardir, 'packages', 'bin', 'release'), dst="llplugin"):
-                self.path("d3dcompiler_43.dll")
                 self.path("d3dcompiler_47.dll")
                 self.path("libcef.dll")
                 self.path("libEGL.dll")

3cf29e28f1549f04c2d2c9e2cde079c3c1ce64ce
Cinder <cinder@sdf.org>
2016-07-23 22:18:02 -0500

    yes bb

--- indra/llmessage/message.cpp
+++ indra/llmessage/message.cpp
@@ -4066,7 +4066,7 @@ void LLMessageSystem::sendUntrustedSimulatorMessageCoro(std::string url, std::st
     LLSD httpResults = result[LLCoreHttpUtil::HttpCoroutineAdapter::HTTP_RESULTS];
     LLCore::HttpStatus status = LLCoreHttpUtil::HttpCoroutineAdapter::getStatusFromLLSD(httpResults);
 
-    if (callback)
+    if (callback && callback != nullptr)
         callback((status) ? LL_ERR_NOERR : LL_ERR_TCP_TIMEOUT);
 }
 

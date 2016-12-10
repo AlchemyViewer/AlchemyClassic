6030f75c2181ddecfff0d13c5e422800aa9715d3
Drake Arconis <drake@alchemyviewer.org>
2016-12-10 01:30:18 -0500

    Fix a bug in LLLineEditor where as capslock was clearing the selection box

--- indra/llui/lllineeditor.cpp
+++ indra/llui/lllineeditor.cpp
@@ -1566,7 +1566,7 @@ BOOL LLLineEditor::handleKeyHere(KEY key, MASK mask )
 				KEY_SHIFT != key &&
 				KEY_CONTROL != key &&
 				KEY_ALT != key &&
-				KEY_CAPSLOCK )
+				KEY_CAPSLOCK != key)
 			{
 				deselect();
 			}

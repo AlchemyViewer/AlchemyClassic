eb7296019aefffe668d7e118e66b6ea61d8d6a2a
Drake Arconis <drake@alchemyviewer.org>
2017-05-17 09:04:40 -0400

    Add missing header comment to files

--- indra/newview/llfloateravatarrendersettings.cpp
+++ indra/newview/llfloateravatarrendersettings.cpp
@@ -1,3 +1,5 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 /**
  * @file llfloateravatarrendersettings.cpp
  * @brief Shows the list of avatars with non-default rendering settings
--- indra/newview/llfloatergridstatus.cpp
+++ indra/newview/llfloatergridstatus.cpp
@@ -1,3 +1,5 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 /** 
  * @file llfloatergridstatus.cpp
  * @brief Grid status floater - uses an embedded web browser to show Grid status info

380f108245f87e1f5f8324940ccc2a2d66da3040
Drake Arconis <drake@alchemyviewer.org>
2017-06-06 19:00:23 -0400

    Backed out changeset: d710676acc0c

--- indra/llcommon/tests/llbase64_test.cpp
+++ indra/llcommon/tests/llbase64_test.cpp
@@ -1,79 +1,98 @@
 // This is an open source non-commercial project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
-/**
+/** 
  * @file llbase64_test.cpp
- * @author Drake Arconis, James Cook
- * @date 2017-05-29
+ * @author James Cook
+ * @date 2007-02-04
  *
- * $LicenseInfo:firstyear=2017&license=viewerlgpl$
- * Alchemy Viewer Source Code
+ * $LicenseInfo:firstyear=2007&license=viewerlgpl$
+ * Second Life Viewer Source Code
  * Copyright (C) 2010, Linden Research, Inc.
- * Copyright (C) 2017, Drake Arconis
- *
+ * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation;
  * version 2.1 of the License only.
- *
+ * 
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- *
+ * 
  * Linden Research, Inc., 945 Battery Street, San Francisco, CA  94111  USA
  * $/LicenseInfo$
  */
-#include "linden_common.h"
 
 #include <string>
 
-#include "llbase64.h"
-#include "lluuid.h"
+#include "linden_common.h"
 
-#include <gtest/gtest.h>
+#include "../llbase64.h"
+#include "../lluuid.h"
 
+#include "../test/lltut.h"
 
-TEST(LLBase64, encode_nothing)
+namespace tut
 {
-	std::string result = LLBase64::encode(NULL, 0);
-	EXPECT_EQ(LLStringUtil::null, result);
+	struct base64_data
+	{
+	};
+	typedef test_group<base64_data> base64_test;
+	typedef base64_test::object base64_object;
+	tut::base64_test base64("LLBase64");
+
+	template<> template<>
+	void base64_object::test<1>()
+	{
+		std::string result;
+
+		result = LLBase64::encode(NULL, 0);
+		ensure("encode nothing", (result == "") );
+
+		LLUUID nothing;
+		result = LLBase64::encode(&nothing.mData[0], UUID_BYTES);
+		ensure("encode blank uuid",
+				(result == "AAAAAAAAAAAAAAAAAAAAAA==") );
+
+		LLUUID id("526a1e07-a19d-baed-84c4-ff08a488d15e");
+		result = LLBase64::encode(&id.mData[0], UUID_BYTES);
+		ensure("encode random uuid",
+				(result == "UmoeB6Gduu2ExP8IpIjRXg==") );
+
+	}
+
+	template<> template<>
+	void base64_object::test<2>()
+	{
+		std::string result;
+
+		U8 blob[40] = { 115, 223, 172, 255, 140, 70, 49, 125, 236, 155, 45, 199, 101, 17, 164, 131, 230, 19, 80, 64, 112, 53, 135, 98, 237, 12, 26, 72, 126, 14, 145, 143, 118, 196, 11, 177, 132, 169, 195, 134 };
+		result = LLBase64::encode(&blob[0], 40);
+		ensure("encode 40 bytes", 
+				(result == "c9+s/4xGMX3smy3HZRGkg+YTUEBwNYdi7QwaSH4OkY92xAuxhKnDhg==") );
+	}
+	
+	template<> template<>
+	void base64_object::test<3>()
+	{
+		std::string result;
+		
+		result = LLBase64::encode(LLStringExplicit("Hands full of bananas."));
+		ensure("encode string",
+			   (result == "SGFuZHMgZnVsbCBvZiBiYW5hbmFzLg=="));
+	}
+	
+	template<> template<>
+	void base64_object::test<4>()
+	{
+		std::string result;
+		
+		result = LLBase64::decode(LLStringExplicit("SGFuZHMgZnVsbCBvZiBiYW5hbmFzLg=="));
+		ensure("decode string",
+			   (result == "Hands full of bananas."));
+	}
 }
-
-TEST(LLBase64, encode_blank_uuid)
-{
-	LLUUID nothing;
-	std::string result = LLBase64::encode(&nothing.mData[0], UUID_BYTES);
-	EXPECT_EQ(std::string("AAAAAAAAAAAAAAAAAAAAAA=="), result);
-}
-
-TEST(LLBase64, encode_random_uuid)
-{
-	LLUUID id("526a1e07-a19d-baed-84c4-ff08a488d15e");
-	std::string result = LLBase64::encode(&id.mData[0], UUID_BYTES);
-	EXPECT_EQ(std::string("UmoeB6Gduu2ExP8IpIjRXg=="), result);
-}
-
-TEST(LLBase64, encode_40_bytes)
-{
-	U8 blob[40] = { 115, 223, 172, 255, 140, 70, 49, 125, 236, 155, 45, 199, 101, 17, 164, 131, 230, 19, 80, 64, 112, 53, 135, 98, 237, 12, 26, 72, 126, 14, 145, 143, 118, 196, 11, 177, 132, 169, 195, 134 };
-	auto result = LLBase64::encode(&blob[0], 40);
-	EXPECT_EQ(std::string("c9+s/4xGMX3smy3HZRGkg+YTUEBwNYdi7QwaSH4OkY92xAuxhKnDhg=="), result);
-}
-
-TEST(LLBase64, encode_string)
-{
-	std::string result = LLBase64::encode(LLStringExplicit("Hands full of bananas."));
-	EXPECT_EQ(std::string("SGFuZHMgZnVsbCBvZiBiYW5hbmFzLg=="), result);
-}
-
-TEST(LLBase64, decode_string)
-{
-	std::string result = LLBase64::decode(LLStringExplicit("SGFuZHMgZnVsbCBvZiBiYW5hbmFzLg=="));
-	EXPECT_EQ(std::string("Hands full of bananas."), result);
-}
-
-
--- indra/llcommon/tests/llrand_test.cpp
+++ indra/llcommon/tests/llrand_test.cpp
@@ -1,112 +1,129 @@
 // This is an open source non-commercial project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
-/**
+/** 
  * @file llrandom_test.cpp
- * @author Drake Arconis, Phoenix
- * @date 2017-06-06
+ * @author Phoenix
+ * @date 2007-01-25
  *
- * $LicenseInfo:firstyear=2017&license=viewerlgpl$
+ * $LicenseInfo:firstyear=2007&license=viewerlgpl$
  * Second Life Viewer Source Code
  * Copyright (C) 2010, Linden Research, Inc.
- * Copyright (C) 2017, Drake Arconis
- *
+ * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation;
  * version 2.1 of the License only.
- *
+ * 
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- *
+ * 
  * Linden Research, Inc., 945 Battery Street, San Francisco, CA  94111  USA
  * $/LicenseInfo$
  */
 
 #include "linden_common.h"
+#include "../test/lltut.h"
 
 #include "../llrand.h"
 
-#include <gtest/gtest.h>
 
-TEST(LLRand, FloatRandomDefaultRange)
+namespace tut
 {
-	F32 number = 0.0f;
-	for (S32 ii = 0; ii < 100000; ++ii)
+	struct random
 	{
-		number = ll_frand();
-		ASSERT_TRUE(number >= 0.0f);
-		ASSERT_TRUE(number < 1.0f);
+	};
+
+	typedef test_group<random> random_t;
+	typedef random_t::object random_object_t;
+	tut::random_t tut_random("LLSeedRand");
+
+	template<> template<>
+	void random_object_t::test<1>()
+	{
+		F32 number = 0.0f;
+		for(S32 ii = 0; ii < 100000; ++ii)
+		{
+			number = ll_frand();
+			ensure("frand >= 0", (number >= 0.0f));
+			ensure("frand < 1", (number < 1.0f));
+		}
 	}
-}
 
-TEST(LLRand, DoubleRandomDefaultRange)
-{
-	F64 number = 0.0f;
-	for (S32 ii = 0; ii < 100000; ++ii)
+	template<> template<>
+	void random_object_t::test<2>()
 	{
-		number = ll_drand();
-		ASSERT_TRUE(number >= 0.0);
-		ASSERT_TRUE(number < 1.0);
+		F64 number = 0.0f;
+		for(S32 ii = 0; ii < 100000; ++ii)
+		{
+			number = ll_drand();
+			ensure("drand >= 0", (number >= 0.0));
+			ensure("drand < 1", (number < 1.0));
+		}
 	}
-}
 
-TEST(LLRand, NormalizedOneNegPos)
-{
-	F32 number = 0.0f;
-	for (S32 ii = 0; ii < 100000; ++ii)
+	template<> template<>
+	void random_object_t::test<3>()
 	{
-		number = ll_frand(2.0f) - 1.0f;
-		ASSERT_TRUE(number >= -1.0f);
-		ASSERT_TRUE(number <= 1.0f);
+		F32 number = 0.0f;
+		for(S32 ii = 0; ii < 100000; ++ii)
+		{
+			number = ll_frand(2.0f) - 1.0f;
+			ensure("frand >= 0", (number >= -1.0f));
+			ensure("frand < 1", (number <= 1.0f));
+		}
 	}
-}
 
-TEST(LLRand, NegativeFloat)
-{
-	F32 number = 0.0f;
-	for (S32 ii = 0; ii < 100000; ++ii)
+	template<> template<>
+	void random_object_t::test<4>()
 	{
-		number = ll_frand(-7.0);
-		ASSERT_TRUE(number <= 0.0);
-		ASSERT_TRUE(number > -7.0);
+		F32 number = 0.0f;
+		for(S32 ii = 0; ii < 100000; ++ii)
+		{
+			number = ll_frand(-7.0);
+			ensure("drand <= 0", (number <= 0.0));
+			ensure("drand > -7", (number > -7.0));
+		}
 	}
-}
 
-TEST(LLRand, NegativeDouble)
-{
-	F64 number = 0.0f;
-	for (S32 ii = 0; ii < 100000; ++ii)
+	template<> template<>
+	void random_object_t::test<5>()
 	{
-		number = ll_drand(-2.0);
-		ASSERT_TRUE(number <= 0.0);
-		ASSERT_TRUE(number > -2.0);
+		F64 number = 0.0f;
+		for(S32 ii = 0; ii < 100000; ++ii)
+		{
+			number = ll_drand(-2.0);
+			ensure("drand <= 0", (number <= 0.0));
+			ensure("drand > -2", (number > -2.0));
+		}
 	}
-}
 
-TEST(LLRand, PositiveInteger)
-{
-	S32 number = 0;
-	for (S32 ii = 0; ii < 100000; ++ii)
+	template<> template<>
+	void random_object_t::test<6>()
 	{
-		number = ll_rand(100);
-		ASSERT_TRUE(number >= 0);
-		ASSERT_TRUE(number < 100);
+		S32 number = 0;
+		for(S32 ii = 0; ii < 100000; ++ii)
+		{
+			number = ll_rand(100);
+			ensure("rand >= 0", (number >= 0));
+			ensure("rand < 100", (number < 100));
+		}
 	}
-}
 
-TEST(LLRand, NegativeInteger)
-{
-	S32 number = 0;
-	for (S32 ii = 0; ii < 100000; ++ii)
+	template<> template<>
+	void random_object_t::test<7>()
 	{
-		number = ll_rand(-127);
-		ASSERT_TRUE(number <= 0);
-		ASSERT_TRUE(number > -127);
+		S32 number = 0;
+		for(S32 ii = 0; ii < 100000; ++ii)
+		{
+			number = ll_rand(-127);
+			ensure("rand <= 0", (number <= 0));
+			ensure("rand > -127", (number > -127));
+		}
 	}
 }

dd07b42d7ef7478b28aa31677d79fda8b8e47380
Drake Arconis <drake@alchemyviewer.org>
2017-08-03 09:56:26 -0400

    Backed out changeset: bb1ffc690e66

--- indra/newview/llinventorymodelbackgroundfetch.cpp
+++ indra/newview/llinventorymodelbackgroundfetch.cpp
@@ -77,13 +77,6 @@
 // * Review the download rate throttling.  Slow then fast?
 //   Detect bandwidth usage and speed up when it drops?
 //
-// * A lot of calls to notifyObservers().  It looks like
-//   these could be collapsed by maintaining a 'dirty'
-//   bit and there appears to be an attempt to do this.
-//   But it isn't used or is used in a limited fashion.
-//   Are there semanic issues requiring a call after certain
-//   updateItem() calls?
-//
 // * An error on a fetch could be due to one item in the batch.
 //   If the batch were broken up, perhaps more of the inventory
 //   would download.  (Handwave here, not certain this is an
@@ -394,6 +387,12 @@ void LLInventoryModelBackgroundFetch::bulkFetch()
 	{
 		// Process completed background HTTP requests
 		gInventory.handleResponses(false);
+		// Just processed a bunch of items.
+		// Note: do we really need notifyObservers() here?
+		// OnIdle it will be called anyway due to Add flag for processed item.
+		// It seems like in some cases we are updaiting on fail (no flag),
+		// but is there anything to update?
+		gInventory.notifyObservers();
 	}
 	
 	if ((mFetchCount > max_concurrent_fetches) ||
@@ -712,7 +711,6 @@ void BGFolderHttpHandler::processData(LLSD & content, LLCore::HttpResponse * res
                         titem->setParent(lost_uuid);
                         titem->updateParentOnServer(FALSE);
                         gInventory.updateItem(titem);
-                        gInventory.notifyObservers();
                     }
                 }
             }
@@ -785,8 +783,6 @@ void BGFolderHttpHandler::processData(LLSD & content, LLCore::HttpResponse * res
 	{
 		fetcher->setAllFoldersFetched();
 	}
-	
-	gInventory.notifyObservers();
 }
 
 
@@ -829,7 +825,6 @@ void BGFolderHttpHandler::processFailure(LLCore::HttpStatus status, LLCore::Http
 			fetcher->setAllFoldersFetched();
 		}
 	}
-	gInventory.notifyObservers();
 }
 
 
@@ -867,7 +862,6 @@ void BGFolderHttpHandler::processFailure(const char * const reason, LLCore::Http
 			fetcher->setAllFoldersFetched();
 		}
 	}
-	gInventory.notifyObservers();
 }
 
 

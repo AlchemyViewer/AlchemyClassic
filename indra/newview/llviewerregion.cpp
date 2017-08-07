cc497fda4dfbd5df81856555ecb570ccb221fc17
Drake Arconis <drake@alchemyviewer.org>
2017-08-07 08:34:22 -0400

    Fix bandwith stats insane value due to incorrect manual conversion of LLUnit type resulting in incorrect result

--- indra/newview/llviewerregion.cpp
+++ indra/newview/llviewerregion.cpp
@@ -1886,7 +1886,7 @@ void LLViewerRegion::updateNetStats()
 	mLastPacketsLost =	mPacketsLost;
 
 	mPacketsIn =				cdp->getPacketsIn();
-	mBitsIn =					8 * cdp->getBytesIn();
+	mBitsIn =					cdp->getBytesIn();
 	mPacketsOut =				cdp->getPacketsOut();
 	mPacketsLost =				cdp->getPacketsLost();
 	mPingDelay =				cdp->getPingDelay();

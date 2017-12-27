7849aaebbb1af63721971c3bef717a4eaf7cfd96
Luminous Luminos <luminous@alchemyviewer.org>
2017-12-26 22:58:43 -0600

    macOS lib updates and sdk deployment target 10.13

--- indra/cmake/Variables.cmake
+++ indra/cmake/Variables.cmake
@@ -182,7 +182,7 @@ if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
     message( FATAL_ERROR "Xcode 8.0.0 or greater is required." )
   endif (XCODE_VERSION LESS 8.0.0)
   message( "Building with " ${CMAKE_OSX_SYSROOT} )
-  set(CMAKE_OSX_DEPLOYMENT_TARGET 10.8)
+  set(CMAKE_OSX_DEPLOYMENT_TARGET 10.13)
 
   set(CMAKE_XCODE_ATTRIBUTE_GCC_VERSION "com.apple.compilers.llvm.clang.1_0")
   set(CMAKE_XCODE_ATTRIBUTE_GCC_OPTIMIZATION_LEVEL 3)

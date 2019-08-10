a95146aa7ca7f69d207cd0b3e5e26177cec6bb99
Rye Mutt <rye@bred.dog>
2019-08-10 10:32:14 -0400

    Okay its gone now.

--- indra/newview/alviewermenu.cpp
+++ indra/newview/alviewermenu.cpp
@@ -1,5 +1,3 @@
-// This is an open source non-commercial project. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 /**
 * @file alviewermenu.cpp
 * @brief Builds menus out of items. Imagine the fast, easy, fun Alchemy style
--- indra/newview/llfloateravatarrendersettings.cpp
+++ indra/newview/llfloateravatarrendersettings.cpp
@@ -1,5 +1,3 @@
-// This is an open source non-commercial project. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 /**
  * @file llfloateravatarrendersettings.cpp
  * @brief Shows the list of avatars with non-default rendering settings

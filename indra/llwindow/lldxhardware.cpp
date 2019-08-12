ac97df3977b98c48eb29344f75acba61775ee637
Rye Mutt <rye@bred.dog>
2019-08-11 22:31:22 -0400

    Use DXGI to get vram on windows

--- indra/llwindow/lldxhardware.cpp
+++ indra/llwindow/lldxhardware.cpp
@@ -34,6 +34,7 @@
 #include <dxdiag.h>
 #undef INITGUID
 
+#include <dxgi.h>
 #include <wbemidl.h>
 #include <comdef.h>
 
@@ -60,150 +61,6 @@ LLDXHardware gDXHardware;
 typedef BOOL ( WINAPI* PfnCoSetProxyBlanket )( IUnknown* pProxy, DWORD dwAuthnSvc, DWORD dwAuthzSvc,
                                                OLECHAR* pServerPrincName, DWORD dwAuthnLevel, DWORD dwImpLevel,
                                                RPC_AUTH_IDENTITY_HANDLE pAuthInfo, DWORD dwCapabilities );
-
-HRESULT GetVideoMemoryViaWMI( WCHAR* strInputDeviceID, DWORD* pdwAdapterRam )
-{
-    HRESULT hr;
-    bool bGotMemory = false;
-    HRESULT hrCoInitialize = S_OK;
-    IWbemLocator* pIWbemLocator = nullptr;
-    IWbemServices* pIWbemServices = nullptr;
-    BSTR pNamespace = nullptr;
-
-    *pdwAdapterRam = 0;
-    hrCoInitialize = CoInitialize( nullptr );
-
-    hr = CoCreateInstance( CLSID_WbemLocator,
-                           nullptr,
-                           CLSCTX_INPROC_SERVER,
-                           IID_IWbemLocator,
-                           ( LPVOID* )&pIWbemLocator );
-#ifdef PRINTF_DEBUGGING
-    if( FAILED( hr ) ) wprintf( L"WMI: CoCreateInstance failed: 0x%0.8x\n", hr );
-#endif
-
-    if( SUCCEEDED( hr ) && pIWbemLocator )
-    {
-        // Using the locator, connect to WMI in the given namespace.
-        pNamespace = SysAllocString( L"\\\\.\\root\\cimv2" );
-
-        hr = pIWbemLocator->ConnectServer( pNamespace, nullptr, nullptr, nullptr,
-                                           0L, nullptr, nullptr, &pIWbemServices );
-#ifdef PRINTF_DEBUGGING
-        if( FAILED( hr ) ) wprintf( L"WMI: pIWbemLocator->ConnectServer failed: 0x%0.8x\n", hr );
-#endif
-        if( SUCCEEDED( hr ) && pIWbemServices != nullptr )
-        {
-            HINSTANCE hinstOle32 = LoadLibraryW( L"ole32.dll" );
-            if( hinstOle32 )
-            {
-                PfnCoSetProxyBlanket pfnCoSetProxyBlanket = nullptr;
-
-                pfnCoSetProxyBlanket = ( PfnCoSetProxyBlanket )GetProcAddress( hinstOle32, "CoSetProxyBlanket" );
-                if( pfnCoSetProxyBlanket != nullptr )
-                {
-                    // Switch security level to IMPERSONATE. 
-                    pfnCoSetProxyBlanket( pIWbemServices, RPC_C_AUTHN_WINNT, RPC_C_AUTHZ_NONE, nullptr,
-                                          RPC_C_AUTHN_LEVEL_CALL, RPC_C_IMP_LEVEL_IMPERSONATE, nullptr, 0 );
-                }
-
-                FreeLibrary( hinstOle32 );
-            }
-
-            IEnumWbemClassObject* pEnumVideoControllers = nullptr;
-            BSTR pClassName = nullptr;
-
-            pClassName = SysAllocString( L"Win32_VideoController" );
-
-            hr = pIWbemServices->CreateInstanceEnum( pClassName, 0,
-                                                     nullptr, &pEnumVideoControllers );
-#ifdef PRINTF_DEBUGGING
-            if( FAILED( hr ) ) wprintf( L"WMI: pIWbemServices->CreateInstanceEnum failed: 0x%0.8x\n", hr );
-#endif
-
-            if( SUCCEEDED( hr ) && pEnumVideoControllers )
-            {
-                IWbemClassObject* pVideoControllers[10] = {nullptr};
-                DWORD uReturned = 0;
-                BSTR pPropName = nullptr;
-
-                // Get the first one in the list
-                pEnumVideoControllers->Reset();
-                hr = pEnumVideoControllers->Next( 5000,             // timeout in 5 seconds
-                                                  10,                  // return the first 10
-                                                  pVideoControllers,
-                                                  &uReturned );
-#ifdef PRINTF_DEBUGGING
-                if( FAILED( hr ) ) wprintf( L"WMI: pEnumVideoControllers->Next failed: 0x%0.8x\n", hr );
-                if( uReturned == 0 ) wprintf( L"WMI: pEnumVideoControllers uReturned == 0\n" );
-#endif
-
-                VARIANT var;
-                if( SUCCEEDED( hr ) )
-                {
-                    bool bFound = false;
-                    for( UINT iController = 0; iController < uReturned; iController++ )
-                    {
-                        if ( !pVideoControllers[iController] )
-                            continue;
-
-                        pPropName = SysAllocString( L"PNPDeviceID" );
-                        hr = pVideoControllers[iController]->Get( pPropName, 0L, &var, nullptr, nullptr );
-#ifdef PRINTF_DEBUGGING
-                        if( FAILED( hr ) )
-                            wprintf( L"WMI: pVideoControllers[iController]->Get PNPDeviceID failed: 0x%0.8x\n", hr );
-#endif
-                        if( SUCCEEDED( hr ) )
-                        {
-                            if( wcsstr( var.bstrVal, strInputDeviceID ) != nullptr )
-                                bFound = true;
-                        }
-                        VariantClear( &var );
-                        if( pPropName ) SysFreeString( pPropName );
-
-                        if( bFound )
-                        {
-                            pPropName = SysAllocString( L"AdapterRAM" );
-                            hr = pVideoControllers[iController]->Get( pPropName, 0L, &var, nullptr, nullptr );
-#ifdef PRINTF_DEBUGGING
-                            if( FAILED( hr ) )
-                                wprintf( L"WMI: pVideoControllers[iController]->Get AdapterRAM failed: 0x%0.8x\n",
-                                         hr );
-#endif
-                            if( SUCCEEDED( hr ) )
-                            {
-                                bGotMemory = true;
-                                *pdwAdapterRam = var.ulVal;
-                            }
-                            VariantClear( &var );
-                            if( pPropName ) SysFreeString( pPropName );
-                            break;
-                        }
-                        SAFE_RELEASE( pVideoControllers[iController] );
-                    }
-                }
-                SAFE_RELEASE( pEnumVideoControllers );
-            }
-
-            if( pClassName )
-                SysFreeString( pClassName );
-        }
-
-        if( pNamespace )
-            SysFreeString( pNamespace );
-        SAFE_RELEASE( pIWbemServices );
-    }
-
-    SAFE_RELEASE( pIWbemLocator );
-
-    if( SUCCEEDED( hrCoInitialize ) )
-        CoUninitialize();
-
-    if( bGotMemory )
-        return S_OK;
-    else
-        return E_FAIL;
-}
 
 //Getting the version of graphics controller driver via WMI
 std::string LLDXHardware::getDriverVersionWMI()
@@ -541,325 +398,58 @@ LLDXHardware::LLDXHardware()
 
 void LLDXHardware::cleanup()
 {
-  // for_each(mDevices.begin(), mDevices.end(), DeletePairedPointer());
-  // mDevices.clear();
 }
 
-/*
-std::string LLDXHardware::dumpDevices()
+
+BOOL LLDXHardware::updateVRAM()
 {
-	if (gWriteDebug)
-	{
-		gWriteDebug("\n");
-		gWriteDebug("StartAllDevices\n");
-	}
-	for (device_map_t::iterator iter = mDevices.begin(),
-			 end = mDevices.end();
-		 iter != end; iter++)
-	{
-		LLDXDevice *devicep = iter->second;
-		devicep->dump();
-	}
-	if (gWriteDebug)
-	{
-		gWriteDebug("EndAllDevices\n\n");
-	}
-	return "";
-}
+	HRESULT hr;
+	BOOL bGotMemory = FALSE;
+	HRESULT hrCoInitialize = S_OK;
+	SIZE_T vram = 0;
 
-LLDXDevice *LLDXHardware::findDevice(const std::string &vendor, const std::string &devices)
-{
-	// Iterate through different devices tokenized in devices string
-	std::string str(devices);
-	typedef boost::tokenizer<boost::char_separator<char> > tokenizer;
-	boost::char_separator<char> sep("|", "", boost::keep_empty_tokens);
-	tokenizer tokens(str, sep);
-
-	tokenizer::iterator iter = tokens.begin();
-	for (;iter != tokens.end();++iter)
+	hrCoInitialize = CoInitialize(0);
+	if (SUCCEEDED(hrCoInitialize))
 	{
-		std::string dev_str = *iter;
-		for (device_map_t::iterator iter = mDevices.begin(),
-				 end = mDevices.end();
-			 iter != end; iter++)
+		IDXGIFactory1* pDXGIFactory = nullptr;
+
+		hr = CreateDXGIFactory1(IID_PPV_ARGS(&pDXGIFactory));
+		if (SUCCEEDED(hr))
 		{
-			LLDXDevice *devicep = iter->second;
-			if ((devicep->mVendorID == vendor)
-				&& (devicep->mDeviceID == dev_str))
+			assert(pDXGIFactory != 0);
+
+			for (UINT index = 0; ; ++index)
 			{
-				return devicep;
-			}
-		}
-	}
+				IDXGIAdapter1* pAdapter = nullptr;
+				hr = pDXGIFactory->EnumAdapters1(index, &pAdapter);
+				if (FAILED(hr)) // DXGIERR_NOT_FOUND is expected when the end of the list is hit
+					break;
 
-	return NULL;
-}
-*/
-
-BOOL LLDXHardware::getInfo(BOOL vram_only)
-{
-	LLTimer hw_timer;
-	BOOL ok = FALSE;
-    HRESULT       hr;
-
-    hr = CoInitialize(nullptr);
-	if (FAILED(hr))
-	{
-		LL_WARNS() << "COM initialization failure!" << LL_ENDL;
-		gWriteDebug("COM initialization failure!\n");
-		return ok;
-	}
-
-    IDxDiagProvider *dx_diag_providerp = nullptr;
-    IDxDiagContainer *dx_diag_rootp = nullptr;
-	IDxDiagContainer *devices_containerp = nullptr;
-	// IDxDiagContainer *system_device_containerp= NULL;
-	IDxDiagContainer *device_containerp = nullptr;
-	//IDxDiagContainer *file_containerp = NULL;
-	IDxDiagContainer *driver_containerp = nullptr;
-
-    // CoCreate a IDxDiagProvider*
-	LL_DEBUGS("AppInit") << "CoCreateInstance IID_IDxDiagProvider" << LL_ENDL;
-    hr = CoCreateInstance(CLSID_DxDiagProvider,
-                          nullptr,
-                          CLSCTX_INPROC_SERVER,
-                          IID_IDxDiagProvider,
-                          (LPVOID*) &dx_diag_providerp);
-
-	if (FAILED(hr))
-	{
-		LL_WARNS("AppInit") << "No DXDiag provider found!  DirectX 9 not installed!" << LL_ENDL;
-		gWriteDebug("No DXDiag provider found!  DirectX 9 not installed!\n");
-		goto LCleanup;
-	}
-    if (SUCCEEDED(hr)) // if FAILED(hr) then dx9 is not installed
-    {
-        // Fill out a DXDIAG_INIT_PARAMS struct and pass it to IDxDiagContainer::Initialize
-        // Passing in TRUE for bAllowWHQLChecks, allows dxdiag to check if drivers are 
-        // digital signed as logo'd by WHQL which may connect via internet to update 
-        // WHQL certificates.    
-        DXDIAG_INIT_PARAMS dx_diag_init_params;
-        ZeroMemory(&dx_diag_init_params, sizeof(DXDIAG_INIT_PARAMS));
-
-        dx_diag_init_params.dwSize                  = sizeof(DXDIAG_INIT_PARAMS);
-        dx_diag_init_params.dwDxDiagHeaderVersion   = DXDIAG_DX9_SDK_VERSION;
-        dx_diag_init_params.bAllowWHQLChecks        = TRUE;
-        dx_diag_init_params.pReserved               = nullptr;
-
-		LL_DEBUGS("AppInit") << "dx_diag_providerp->Initialize" << LL_ENDL;
-        hr = dx_diag_providerp->Initialize(&dx_diag_init_params);
-        if(FAILED(hr))
-		{
-            goto LCleanup;
-		}
-
-		LL_DEBUGS("AppInit") << "dx_diag_providerp->GetRootContainer" << LL_ENDL;
-        hr = dx_diag_providerp->GetRootContainer( &dx_diag_rootp );
-        if(FAILED(hr) || !dx_diag_rootp)
-		{
-            goto LCleanup;
-		}
-
-		// Get display driver information
-		LL_DEBUGS("AppInit") << "dx_diag_rootp->GetChildContainer" << LL_ENDL;
-		hr = dx_diag_rootp->GetChildContainer(L"DxDiag_DisplayDevices", &devices_containerp);
-		if(FAILED(hr) || !devices_containerp)
-		{
-            goto LCleanup;
-		}
-
-		// Get device 0
-		LL_DEBUGS("AppInit") << "devices_containerp->GetChildContainer" << LL_ENDL;
-		hr = devices_containerp->GetChildContainer(L"0", &device_containerp);
-		if(FAILED(hr) || !device_containerp)
-		{
-            goto LCleanup;
-		}
-		
-		DWORD vram = 0;
-
-		WCHAR deviceID[512];
-
-		get_wstring(device_containerp, L"szDeviceID", deviceID, 512);
-		
-		if (SUCCEEDED(GetVideoMemoryViaWMI(deviceID, &vram))) 
-		{
-			mVRAM = vram/(1024*1024);
-		}
-		else
-		{ // Get the English VRAM string
-		  std::string ram_str = get_string(device_containerp, L"szDisplayMemoryEnglish");
-
-		  // We don't need the device any more
-		  SAFE_RELEASE(device_containerp);
-
-		  // Dump the string as an int into the structure
-		  char *stopstring;
-		  mVRAM = strtol(ram_str.c_str(), &stopstring, 10); 
-		  LL_INFOS("AppInit") << "VRAM Detected: " << mVRAM << " DX9 string: " << ram_str << LL_ENDL;
-		}
-
-		if (vram_only)
-		{
-			ok = TRUE;
-			goto LCleanup;
-		}
-
-
-		/* for now, we ONLY do vram_only the rest of this
-		   is commented out, to ensure no-one is tempted
-		   to use it
-		
-		// Now let's get device and driver information
-		// Get the IDxDiagContainer object called "DxDiag_SystemDevices".
-		// This call may take some time while dxdiag gathers the info.
-		DWORD num_devices = 0;
-	    WCHAR wszContainer[256];
-		LL_DEBUGS("AppInit") << "dx_diag_rootp->GetChildContainer DxDiag_SystemDevices" << LL_ENDL;
-		hr = dx_diag_rootp->GetChildContainer(L"DxDiag_SystemDevices", &system_device_containerp);
-		if (FAILED(hr))
-		{
-			goto LCleanup;
-		}
-
-		hr = system_device_containerp->GetNumberOfChildContainers(&num_devices);
-		if (FAILED(hr))
-		{
-			goto LCleanup;
-		}
-
-		LL_DEBUGS("AppInit") << "DX9 iterating over devices" << LL_ENDL;
-		S32 device_num = 0;
-		for (device_num = 0; device_num < (S32)num_devices; device_num++)
-		{
-			hr = system_device_containerp->EnumChildContainerNames(device_num, wszContainer, 256);
-			if (FAILED(hr))
-			{
-				goto LCleanup;
-			}
-
-			hr = system_device_containerp->GetChildContainer(wszContainer, &device_containerp);
-			if (FAILED(hr) || device_containerp == NULL)
-			{
-				goto LCleanup;
-			}
-
-			std::string device_name = get_string(device_containerp, L"szDescription");
-
-			std::string device_id = get_string(device_containerp, L"szDeviceID");
-
-			LLDXDevice *dxdevicep = new LLDXDevice;
-			dxdevicep->mName = device_name;
-			dxdevicep->mPCIString = device_id;
-			mDevices[dxdevicep->mPCIString] = dxdevicep;
-
-			// Split the PCI string based on vendor, device, subsys, rev.
-			std::string str(device_id);
-			typedef boost::tokenizer<boost::char_separator<char> > tokenizer;
-			boost::char_separator<char> sep("&\\", "", boost::keep_empty_tokens);
-			tokenizer tokens(str, sep);
-
-			tokenizer::iterator iter = tokens.begin();
-			S32 count = 0;
-			BOOL valid = TRUE;
-			for (;(iter != tokens.end()) && (count < 3);++iter)
-			{
-				switch (count)
+				DXGI_ADAPTER_DESC1 desc;
+				memset(&desc, 0, sizeof(DXGI_ADAPTER_DESC1));
+				if (SUCCEEDED(pAdapter->GetDesc1(&desc)))
 				{
-				case 0:
-					if (strcmp(iter->c_str(), "PCI"))
+					if (desc.Flags == 0)
 					{
-						valid = FALSE;
+						vram = desc.DedicatedVideoMemory;
+						bGotMemory = TRUE;
 					}
-					break;
-				case 1:
-					dxdevicep->mVendorID = iter->c_str();
-					break;
-				case 2:
-					dxdevicep->mDeviceID = iter->c_str();
-					break;
-				default:
-					// Ignore it
-					break;
 				}
-				count++;
+				SAFE_RELEASE(pAdapter);
+				if (bGotMemory != FALSE)
+					break;
 			}
-
-
-
-
-			// Now, iterate through the related drivers
-			hr = device_containerp->GetChildContainer(L"Drivers", &driver_containerp);
-			if (FAILED(hr) || !driver_containerp)
-			{
-				goto LCleanup;
-			}
-
-			DWORD num_files = 0;
-			hr = driver_containerp->GetNumberOfChildContainers(&num_files);
-			if (FAILED(hr))
-			{
-				goto LCleanup;
-			}
-
-			S32 file_num = 0;
-			for (file_num = 0; file_num < (S32)num_files; file_num++ )
-			{
-
-				hr = driver_containerp->EnumChildContainerNames(file_num, wszContainer, 256);
-				if (FAILED(hr))
-				{
-					goto LCleanup;
-				}
-
-				hr = driver_containerp->GetChildContainer(wszContainer, &file_containerp);
-				if (FAILED(hr) || file_containerp == NULL)
-				{
-					goto LCleanup;
-				}
-
-				std::string driver_path = get_string(file_containerp, L"szPath");
-				std::string driver_name = get_string(file_containerp, L"szName");
-				std::string driver_version = get_string(file_containerp, L"szVersion");
-				std::string driver_date = get_string(file_containerp, L"szDatestampEnglish");
-
-				LLDXDriverFile *dxdriverfilep = new LLDXDriverFile;
-				dxdriverfilep->mName = driver_name;
-				dxdriverfilep->mFilepath= driver_path;
-				dxdriverfilep->mVersionString = driver_version;
-				dxdriverfilep->mVersion.set(driver_version);
-				dxdriverfilep->mDateString = driver_date;
-
-				dxdevicep->mDriverFiles[driver_name] = dxdriverfilep;
-
-				SAFE_RELEASE(file_containerp);
-			}
-			SAFE_RELEASE(device_containerp);
+			SAFE_RELEASE(pDXGIFactory);
 		}
-		*/
-    }
-
-    // dumpDevices();
-    ok = TRUE;
-	
-LCleanup:
-	if (!ok)
-	{
-		LL_WARNS("AppInit") << "DX9 probe failed" << LL_ENDL;
-		gWriteDebug("DX9 probe failed\n");
+		CoUninitialize();
 	}
 
-	//SAFE_RELEASE(file_containerp);
-	SAFE_RELEASE(driver_containerp);
-	SAFE_RELEASE(device_containerp);
-	SAFE_RELEASE(devices_containerp);
-    SAFE_RELEASE(dx_diag_rootp);
-    SAFE_RELEASE(dx_diag_providerp);
-    
-    CoUninitialize();
-    
-    return ok;
-    }
+	if (bGotMemory != FALSE)
+	{
+		mVRAM = vram / (1024 * 1024);
+	}
+	return bGotMemory;
+}
 
 LLSD LLDXHardware::getDisplayInfo()
 {
@@ -906,8 +496,8 @@ LLSD LLDXHardware::getDisplayInfo()
 
         dx_diag_init_params.dwSize                  = sizeof(DXDIAG_INIT_PARAMS);
         dx_diag_init_params.dwDxDiagHeaderVersion   = DXDIAG_DX9_SDK_VERSION;
-        dx_diag_init_params.bAllowWHQLChecks        = FALSE;
-        dx_diag_init_params.pReserved               = nullptr;
+        dx_diag_init_params.bAllowWHQLChecks        = TRUE;
+        dx_diag_init_params.pReserved               = NULL;
 
 		LL_INFOS() << "dx_diag_providerp->Initialize" << LL_ENDL;
         hr = dx_diag_providerp->Initialize(&dx_diag_init_params);

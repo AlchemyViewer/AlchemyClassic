2a37f5193932ee2330a80ced0e328dc62041c4c9
Cinder Biscuits <cinder@cinderblocks.biz>
2019-09-23 08:55:41 -0500

    Common pch  improvements

--- indra/llcommon/llcommonprecompiled.h
+++ indra/llcommon/llcommonprecompiled.h
@@ -1,9 +1,37 @@
+/**
+ * @file llcommonprecompiled.h
+ * @brief Includes common headers
+ *
+ * $LicenseInfo:firstyear=2019&license=viewerlgpl$
+ * Alchemy Viewer Source Code
+ * Copyright (C) 2019, Alchemy Viewer Project, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation;
+ * version 2.1 of the License only.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Linden Research, Inc., 945 Battery Street, San Francisco, CA  94111  USA
+ * $/LicenseInfo$
+ */
+
 #ifndef LL_LLCOMMONPRECOMPILED_H
 #define LL_LLCOMMONPRECOMPILED_H
 
 #include "linden_common.h"
 
-#include "llwin32headerslean.h"
+#ifdef LL_WINDOWS
+#  include "llwin32headerslean.h"
+#endif // LL_WINDOWS
 
 #include <algorithm>
 #include <atomic>
@@ -12,7 +40,6 @@
 #include <deque>
 #include <functional>
 #include <iomanip>
-#include <iosfwd>
 #include <iostream>
 #include <list>
 #include <map>

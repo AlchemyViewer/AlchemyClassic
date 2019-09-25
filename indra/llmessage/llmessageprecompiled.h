23697782d63ed7fb25126c06afd36984d66fa00f
Rye Mutt <rye@alchemyviewer.org>
2019-09-24 21:13:08 -0400

    This cut 15s from llmessage build time

---
+++ indra/llmessage/llmessageprecompiled.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "linden_common.h"
+
+#include "llwin32headerslean.h"
+
+#include "llapr.h"
+#include "llsd.h"
+#include "llsingleton.h"
+#include "llstring.h"
+#include "llstringtable.h"
+#include "llstl.h"
+#include "lltimer.h"

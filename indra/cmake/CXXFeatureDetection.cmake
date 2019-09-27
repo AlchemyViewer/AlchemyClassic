9f22753538e8fe46925c10e6ca0f67318fbdc4ef
Rye Mutt <rye@alchemyviewer.org>
2019-09-27 04:21:35 -0400

    Oops I broke the build

--- indra/cmake/CXXFeatureDetection.cmake
+++ indra/cmake/CXXFeatureDetection.cmake
@@ -8,7 +8,7 @@ include(WriteCompilerDetectionHeader)
 write_compiler_detection_header(
   FILE "${CMAKE_CURRENT_BINARY_DIR}/generated/llcompilerfeatures.h"
   PREFIX LL
-  COMPILERS AppleClang Clang GNU MSVC
+  COMPILERS AppleClang Clang GNU MSVC Intel
   FEATURES
   cxx_alignas cxx_alignof cxx_attribute_deprecated cxx_decltype cxx_decltype_auto cxx_nullptr cxx_override cxx_thread_local cxx_range_for cxx_static_assert
 )

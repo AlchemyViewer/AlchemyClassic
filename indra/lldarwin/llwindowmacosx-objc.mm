bcd2ee4ae4fe041521e08425d3de31ab891bf6f1
Rye Mutt <rye@alchemyviewer.org>
2019-10-15 00:39:52 -0400

    Get the windows backing scale factor for device unit size on mac as thats what our implementation expects

--- indra/lldarwin/llwindowmacosx-objc.mm
+++ indra/lldarwin/llwindowmacosx-objc.mm
@@ -240,7 +240,7 @@ unsigned long getVramSize(GLViewRef view)
 
 float getDeviceUnitSize(GLViewRef view)
 {
-	return [(__bridge LLOpenGLView*)view convertSizeToBacking:NSMakeSize(1, 1)].width;
+    return ((__bridge LLOpenGLView*)view).window.backingScaleFactor;
 }
 
 void getContentViewBounds(NSWindowRef window, float* bounds)

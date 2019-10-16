0b9e22a0d492dd9699b3d495c78f1f3978bfa5b1
Rye Mutt <rye@alchemyviewer.org>
2019-10-15 23:00:56 -0400

    Include viewer bitness in sentry crash data

--- indra/newview/llappviewerwin32.cpp
+++ indra/newview/llappviewerwin32.cpp
@@ -581,6 +581,7 @@ void LLAppViewerWin32::initCrashReporting(bool reportFreeze)
 	annotations.emplace("sentry[contexts][app][app_build]", LLVersionInfo::getChannelAndVersion());
 
 	annotations.emplace("sentry[tags][second_instance]", fmt::to_string(isSecondInstance()));
+	annotations.emplace("sentry[tags][bitness]", fmt::to_string(ADDRESS_SIZE));
 
 	// Optional arguments to pass to the handler
 	std::vector<std::string> arguments;

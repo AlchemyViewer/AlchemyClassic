243a5ffde5e0750b9190588e90bea092520b54b1
Rye Mutt <rye@alchemyviewer.org>
2019-11-11 00:27:43 -0500

    Backed out changeset: 6a1681ccc8f9

--- indra/newview/app_settings/shaders/class1/deferred/softenLightF.glsl
+++ indra/newview/app_settings/shaders/class1/deferred/softenLightF.glsl
@@ -412,27 +412,16 @@ void main()
 
 		if (spec.a > 0.0) // specular reflection
 		{
-			vec3 npos = -normalize(pos.xyz);
- 
-			//vec3 ref = dot(pos+lv, norm);
-			vec3 h = normalize(sun_dir.xyz+npos);
-			float nh = dot(norm.xyz, h);
-			float nv = dot(norm.xyz, npos);
-			float vh = dot(npos, h);
-			float sa = nh;
-			float fres = pow(1 - dot(h, npos), 5)*0.4+0.5;
- 
-			float gtdenom = 2 * nh;
-			float gt = max(0, min(gtdenom * nv / vh, gtdenom * da / vh));
-                                     
-			if (nh > 0.0)
-			{
-				float scol = fres*texture2D(lightFunc, vec2(nh, spec.a)).r*gt/(nh*da);
-				vec3 speccol = (final_da*getSunlitColor())*scol*spec.rgb;
-				speccol = max(speccol, vec3(0));
-				bloom = dot (speccol, speccol) / 6;
-				col += speccol;
-			}
+			// the old infinite-sky shiny reflection
+			//
+			
+			float sa = dot(refnormpersp, sun_dir.xyz);
+			vec3 dumbshiny = vary_SunlitColor*(texture2D(lightFunc, vec2(sa, spec.a)).r);
+			
+			// add the two types of shiny together
+			vec3 spec_contrib = dumbshiny * spec.rgb;
+			bloom = dot(spec_contrib, spec_contrib) / 6;
+			col += spec_contrib;
 		}
 		
 		
